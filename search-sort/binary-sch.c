8b939faa0f2357c1095694b3ff24c5cd91e20920
Sripathy Ramaswamy <sripathyfication@gmail.com>
2014-07-23 13:13:31 -0700

    Folder containing code for search sort algos.

---
+++ search-sort/binary-sch.c
@@ -0,0 +1,17 @@
+/* Program to demonstrate binary search */
+#include <stdio.h>
+
+int arr[6] = {12,14,15,19,21,23};
+
+/* binary search for a number and return its position */
+int binary_search(int num) {
+
+	
+
+}
+
+
+int main() {
+
+
+}
