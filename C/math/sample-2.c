07b8673a58839bd5cfac0ae8d6695e8fa3044dc0
Sripathy Ramaswamy <sripathyfication@gmail.com>
2016-07-25 10:56:58 -0700

    Some deletes, some notes,

--- C/math/sample-2.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(void)
-{
-char a[1000];
-return 0;
-}
---
+++ C/math/single_num.c
@@ -0,0 +1,17 @@
+/*
+ *Given an array where every element occurs three times, except one element which occurs only
+ * once. Find the element that occurs once. Expected time complexity is O(n) and O(1) extra space.
+Examples:
+
+Input: arr[] = {12, 1, 12, 3, 12, 1, 1, 2, 3, 3}
+Output: 2
+
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int find_single_num(int *arr, int size)
+{
+
+
+}
