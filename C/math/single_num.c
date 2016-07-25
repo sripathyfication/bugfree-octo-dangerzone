b11fe61051eb086e7d401228508ea21687ae7be7
Sripathy Ramaswamy <sripathyfication@gmail.com>
2016-07-25 12:05:43 -0700

    Single number algorithm

--- C/math/single_num.c
+++ C/math/single_num.c
@@ -10,8 +10,36 @@ Output: 2
 #include <stdio.h>
 #include <stdlib.h>
 
+static int compare(const void *a, const void *b) {
+          return *(int*)a - *(int*)b;
+}
+
 int find_single_num(int *arr, int size)
 {
+    int i = 0;
+    qsort(arr, size, sizeof(int), compare);
+
+    int k = 3;
+    while (i < size)
+    {
+        if (arr[i] == arr[i+1])
+            i = i + (k);
+        else 
+            break;
+    }
+
+    return arr[i];
+}
 
 
+int main()
+{
+    int arr[10] = {12,1,12,1,1,12,3,2,3,3};
+    int s_num = 0;
+
+    s_num = find_single_num(arr,10);
+
+    printf("Single num is: %d\n", s_num);
+
+    return 0;
 }
