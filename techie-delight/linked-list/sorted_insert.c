dc49895c793ff898448ecc4138129f760e0608b3
Sripathy Ramaswamy <sripathyfication@gmail.com>
2017-05-10 10:02:11 -0700

    sorted insert

---
+++ techie-delight/linked-list/sorted_insert.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* linked list structure */
+struct node{
+    int data;
+    struct node * next;
+} ;
+
+/* head data structure */
+struct node *head;
+
+/* print list */
+void print_list (struct node *head) 
+{
+    struct node *print = head;
+    int count = 0;
+    if (head == NULL) { printf("head is null \n"); return;}
+
+    while (print != NULL) {
+        if (print->next)
+            printf(" %d --> ", print->data);
+        else
+            printf(" %d ", print->data);
+        print = print->next;
+    }
+    
+    printf("\n");
+    return;
+}
+
+/* new node */
+struct node * new_node(int data) {
+
+    struct node *new = (struct node *) malloc (sizeof(struct node));
+    if (new) {
+        new->data = data;
+        new->next = NULL;
+    }
+
+    return new;
+}
+
+
+/* sorted insert: iterate over the list and find the sorted position in the list and insert in
+ * correct position
+ */
+void sorted_insert(struct node *insert, struct node **head)
+{
+    struct node *find = *head;
+    struct node *prev = NULL;
+
+    /* if head is null, insert becomes head */
+    if (*head == NULL) {
+        *head = insert;
+        return;
+    }
+
+    /* else iterate over the list and find */ 
+    while (find->next) {
+        prev = find;
+        if (find->data < insert->data) {
+            find = find->next;
+        } else {
+            break;
+        }
+    }
+
+    /* if find has a next, which means, this new node has to be inserted
+     * in between. If find doesn't have a next, then insert is the next
+     * */
+    if (prev->next) {
+        insert->next = find;
+        prev->next = insert;
+    } else if (prev->next == NULL) {
+        prev->next = insert;
+    }
+
+    return ;
+
+}
+
+/* driver program */
+int main() {
+
+    head = new_node(1);
+
+    head->next = new_node(2);
+    head->next->next = new_node(3);
+    head->next->next->next = new_node(5);
+
+    print_list(head);
+    sorted_insert(new_node(4), &head);
+    print_list(head);
+
+    return 0;
+}
